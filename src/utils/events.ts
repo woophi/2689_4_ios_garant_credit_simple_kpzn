0d0292f957d9515938a0a09ef413df19e3bc1bb7
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-06-19 12:34:08 +0200

    init

---
+++ src/utils/events.ts
@@ -0,0 +1,27 @@
+declare global {
+  interface Window {
+    dataLayer: unknown[];
+  }
+}
+export const sendDataToGA = async (creditSum: number) => {
+  try {
+    const now = new Date();
+    const date = `${now.getFullYear()}-${
+      now.getMonth() + 1
+    }-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+
+    await fetch(
+      'https://script.google.com/macros/s/AKfycbzQjoTYHuwvlYvWtBpu0so_QUEw5LrTfQfPTApvVm1yCWQedrj3wum4J32mmqtrFyT-/exec',
+      {
+        redirect: 'follow',
+        method: 'POST',
+        body: JSON.stringify({ date, credit_sum: creditSum, variant: '2689_4_ios_garant_credit_simple_kpzn' }),
+        headers: {
+          'Content-Type': 'text/plain;charset=utf-8',
+        },
+      },
+    );
+  } catch (error) {
+    console.error('Error!', error);
+  }
+};
